4c0f3b44869769945d4354f79158177f9e8c69af
Rene Mayrhofer <rene@mayrhofer.eu.org>
2005-10-24 14:01:59 +0000

    Fixed the unit test DongleProtocolHandlerTest - the routine was correct, just the test wasn't...
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@36 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/java/RelateAuthentication/src/org/eu/mayrhofer/authentication/test/DongleProtocolHandlerTest.java
+++ src/java/RelateAuthentication/src/org/eu/mayrhofer/authentication/test/DongleProtocolHandlerTest.java
@@ -13,35 +13,36 @@ public class DongleProtocolHandlerTest extends TestCase {
 		byte[] src4 = {(byte) 0xaa};
 		
 		DongleProtocolHandler.addPart(dest, src1, 0, 1);
-		Assert.assertEquals(0x01, dest[0]);
+		Assert.assertEquals((byte) 0x01, dest[0]);
 		DongleProtocolHandler.addPart(dest, src1, 0, 8);
-		Assert.assertEquals(0x01, dest[0]);
-		DongleProtocolHandler.addPart(dest, src1, 8, 9);
-		Assert.assertEquals(0x01, dest[0]);
-		Assert.assertEquals(0x01, dest[1]);
-		Assert.assertEquals(0x01, dest[2]);
+		Assert.assertEquals((byte) 0x01, dest[0]);
+		DongleProtocolHandler.addPart(dest, src1, 8, 10);
+		Assert.assertEquals((byte) 0x01, dest[0]);
+		Assert.assertEquals((byte) 0x01, dest[1]);
+		Assert.assertEquals((byte) 0x02, dest[2]);
 		DongleProtocolHandler.addPart(dest, src1, 12, 6);
-		Assert.assertEquals(0x01, dest[0]);
-		Assert.assertEquals(0x11, dest[1]);
+		Assert.assertEquals((byte) 0x01, dest[0]);
+		Assert.assertEquals((byte) 0x11, dest[1]);
+		Assert.assertEquals((byte) 0x00, dest[2]);
 		DongleProtocolHandler.addPart(dest, src1, 0, 24);
-		Assert.assertEquals(0x01, dest[0]);
-		Assert.assertEquals(0x02, dest[1]);
-		Assert.assertEquals(0x03, dest[2]);
+		Assert.assertEquals((byte) 0x01, dest[0]);
+		Assert.assertEquals((byte) 0x02, dest[1]);
+		Assert.assertEquals((byte) 0x03, dest[2]);
 
 		DongleProtocolHandler.addPart(dest, src2, 16, 4);
-		Assert.assertEquals(0x0f, dest[2]);
+		Assert.assertEquals((byte) 0x0f, dest[2]);
 		DongleProtocolHandler.addPart(dest, src2, 16, 8);
-		Assert.assertEquals(0xff, dest[2]);
+		Assert.assertEquals((byte) 0xff, dest[2]);
 
 		DongleProtocolHandler.addPart(dest, src3, 0, 8);
-		Assert.assertEquals(0xff, dest[0]);
+		Assert.assertEquals((byte) 0xfe, dest[0]);
 
 		DongleProtocolHandler.addPart(dest, src4, 28, 8);
-		Assert.assertEquals(0xa0, dest[3]);
-		Assert.assertEquals(0x0a, dest[4]);
+		Assert.assertEquals((byte) 0xa0, dest[3]);
+		Assert.assertEquals((byte) 0x0a, dest[4]);
 
-		DongleProtocolHandler.addPart(dest, src2, 34, 7);
-		Assert.assertEquals(0xfc, dest[4]);
-		Assert.assertEquals(0x01, dest[5]);
+		DongleProtocolHandler.addPart(dest, src2, 42, 7);
+		Assert.assertEquals((byte) 0xfc, dest[5]);
+		Assert.assertEquals((byte) 0x01, dest[6]);
 	}
 }
