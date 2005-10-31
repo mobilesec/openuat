285b0dcda83fa74a7fc745bfccc98120119033cf
Rene Mayrhofer <rene@mayrhofer.eu.org>
2005-10-31 09:19:03 +0000

    Add debugging output and test code so that the server can also initiate the dongle authentication.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk/src/java/RelateAuthentication@48 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/org/eu/mayrhofer/authentication/RelateAuthenticationProtocol.java
+++ src/org/eu/mayrhofer/authentication/RelateAuthenticationProtocol.java
@@ -192,9 +192,20 @@ public class RelateAuthenticationProtocol extends AuthenticationEventSender {
     public static void main(String[] args) throws Exception
 	{
     	class TempAuthenticationEventHandler implements AuthenticationProgressHandler {
+    		RelateAuthenticationProtocol outer;
+    		
     	    public void AuthenticationSuccess(Object remote, Object result)
     	    {
-    	        System.out.println("Received authentication success event with " + remote + ", got result " + result);
+    	        System.out.println("Received authentication success event with host " + remote);
+    	        byte[][] keys = (byte[][]) result;
+    	        System.out.println("Shared session key is now '" + keys[0] + "' with length " + keys[0].length + ", shared authentication key is now '" + keys[1] + "' with length " + keys[1].length);
+    	        System.out.println("Starting dongle authentication with remote relate id " + outer.remoteRelateId);
+    	        // remember the secret key shared with the other device
+    	        outer.sharedKey = keys[0];
+    	        // and use the agreed authentication key to start the dongle authentication
+    	        DongleProtocolHandler dh = new DongleProtocolHandler(SerialPort, outer.remoteRelateId);
+    	        dh.addAuthenticationProgressHandler(outer.new DongleAuthenticationEventHandler());
+            	dh.startAuthentication(keys[1], outer.rounds);
     	    }
     	    public void AuthenticationFailure(Object remote, Exception e, String msg)
     	    {
@@ -213,17 +224,19 @@ public class RelateAuthenticationProtocol extends AuthenticationEventSender {
     	    }
     	}
     	
-        if (args.length > 0 && args[0].equals("server"))
+        if (args.length > 1 && args[0].equals("server"))
         {
             HostServerSocket h1 = new HostServerSocket(TcpPort);
-        	h1.addAuthenticationProgressHandler(new TempAuthenticationEventHandler());
+            TempAuthenticationEventHandler h = new TempAuthenticationEventHandler();
+            h.outer = new RelateAuthenticationProtocol("", (byte) Integer.parseInt(args[1]));
+        	h1.addAuthenticationProgressHandler(h);
             h1.startListening();
             new BufferedReader(new InputStreamReader(System.in)).readLine();
             h1.stopListening();
         }
-        if (args.length > 1 && args[0].equals("client"))
+        if (args.length > 2 && args[0].equals("client"))
         {
-        	RelateAuthenticationProtocol r = new RelateAuthenticationProtocol(args[1], (byte) 100);
+        	RelateAuthenticationProtocol r = new RelateAuthenticationProtocol(args[1], (byte) Integer.parseInt(args[2]));
         	r.addAuthenticationProgressHandler(new TempAuthenticationEventHandler());
         	r.startAuthentication(2);
             new BufferedReader(new InputStreamReader(System.in)).readLine();
