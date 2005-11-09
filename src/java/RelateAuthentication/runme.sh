9817803d4c835eed322638a34a5d48e969e1e4f3
Rene Mayrhofer <rene@mayrhofer.eu.org>
2005-11-09 16:56:00 +0000

    Add the "." classpath so that the log4j.properties file is found.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@80 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/java/RelateAuthentication/runme.sh
+++ src/java/RelateAuthentication/runme.sh
@@ -1,1 +1,2 @@
-java -cp bin:extlib/comm.jar:extlib/log4j-1.2.jar -Djava.library.path=nativelib/linux/ org.eu.mayrhofer.authentication.RelateAuthenticationProtocol $@
+# classpath . is just so that log4j.properties is found
+java -cp bin:extlib/comm.jar:extlib/log4j-1.2.jar:. -Djava.library.path=nativelib/linux/ org.eu.mayrhofer.authentication.RelateAuthenticationProtocol $@
