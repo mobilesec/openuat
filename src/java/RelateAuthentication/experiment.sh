87b2c96fa5f99c79ba7a136637c3c92ac6306872
Rene Mayrhofer <rene@mayrhofer.eu.org>
2005-11-23 11:57:03 +0000

    Hmm, why didn't that work? Fixed the script to use correct file names.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@123 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/java/RelateAuthentication/experiment.sh
+++ src/java/RelateAuthentication/experiment.sh
@@ -5,9 +5,9 @@ while [ $r -le 43 ]; do
   while [ $i -le 29 ]; do 
     echo -------------------------------------------------
     echo "Rounds: $r Try: $i"
-    if [ ! -e /tmp/exp_$r_$i ]; then
+    if [ ! -e "/tmp/exp_${r}_${i}" ]; then
       ./runme.sh client ind022000011.lancs.ac.uk 3 $r
-      touch /tmp/exp_$r_$i
+      touch "/tmp/exp_${r}_${i}"
     else
       echo "Already done, skipping."
     fi
