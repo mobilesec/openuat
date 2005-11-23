17ff5421d6927a9edb5b04fdd24a1d931e7ead34
Rene Mayrhofer <rene@mayrhofer.eu.org>
2005-11-23 11:55:08 +0000

    New scripts for the experiment.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk/src/java/RelateAuthentication@122 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ experiment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+r=2
+while [ $r -le 43 ]; do 
+  i=0
+  while [ $i -le 29 ]; do 
+    echo -------------------------------------------------
+    echo "Rounds: $r Try: $i"
+    if [ ! -e /tmp/exp_$r_$i ]; then
+      ./runme.sh client ind022000011.lancs.ac.uk 3 $r
+      touch /tmp/exp_$r_$i
+    else
+      echo "Already done, skipping."
+    fi
+    let i=i+1
+  done
+  let r=r+1
+done
