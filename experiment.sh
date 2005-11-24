5324c526e206803861316b40f8303d8b9176aab3
Rene Mayrhofer <rene@mayrhofer.eu.org>
2005-11-24 17:03:17 +0000

    Even more safety belt code: kill the client after 2 minutes.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk/src/java/RelateAuthentication@146 01b9256e-9321-419f-88ee-03bd83f56c6c

--- experiment.sh
+++ experiment.sh
@@ -12,13 +12,20 @@ while [ $r -le 43 ]; do
     echo -------------------------------------------------
     echo "Rounds: $r Try: $i"
     if [ ! -e "/tmp/exp_${r}_${i}" ]; then
-      ./runme.sh client ind022000011.lancs.ac.uk $1 $r
-      touch "/tmp/exp_${r}_${i}"
+      sudo nice -n -10 ./runme.sh client ind022000011.lancs.ac.uk $1 $r
+      ret=$?
       sleep 2s
+      # only mark the run as done for either error or success, but else retry
+      if [ $ret -eq 0 -o $ret -eq 1 ]; then
+        touch "/tmp/exp_${r}_${i}"
+      fi
     else
       echo "Already done, skipping."
+      ret=0
     fi
-    let i=i+1
+    if [ $ret -eq 0 -o $ret -eq 1 ]; then
+      let i=i+1
+    fi
   done
   let r=r+1
 done
