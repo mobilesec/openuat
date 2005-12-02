a5a2caf210b56c400b760966da94ef3a4b6eb97c
Rene Mayrhofer <rene@mayrhofer.eu.org>
2005-12-02 12:07:19 +0000

    - Removed the hacks for the Sun javax.comm API. - Shortened the wait parameters. - Small cosmetical changes. - Prepare to re-run the parameter search experiment.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk/src/java/RelateAuthentication@152 01b9256e-9321-419f-88ee-03bd83f56c6c

--- parameter-search-experiment/parameter-search.sh
+++ parameter-search-experiment/parameter-search.sh
@@ -1,1 +1,21 @@
-magic1=16; while [ $magic1 -le 30 ]; do magic2=50; while [ $magic2 -le 250 ]; do echo -n "$magic1 $magic2 "; i=0; while [ $i -lt 30 ]; do time=`./runme.sh $magic1 $magic2 | awk ' /time to get dongle.s attention/ { print $7; }'`; echo -n "$time "; let i=i+1; done; echo; let magic2=magic2+25; done; let magic1=magic1+1; done | tee attention-magic-values.log.3
+magic1=1
+while [ $magic1 -le 30 ]; do 
+  magic2=50
+  while [ $magic2 -le 400 ]; do 
+    echo -n "$magic1 $magic2: "
+    i=0
+    while [ $i -lt 30 ]; do 
+      time=`java -cp bin:lib/RXTXcomm.jar:lib/log4j-1.2.jar:. \
+            -Dgnu.io.rxtx.SerialPorts=/dev/ttyUSB0:/dev/ttyUSB1 \
+            -Djava.library.path=nativelib/linux uk.ac.lancs.relate.SerialConnector \
+             /dev/ttyUSB1 param-search $magic1 $magic2 2>/dev/null \
+             | awk ' /time to get dongle.s attention/ { print $11; }' \
+             | sed 's/ms//'`
+      echo -n "$time "
+      let i=i+1
+    done
+    echo
+    let magic2=magic2+25
+  done
+  let magic1=magic1+1
+done | tee parameter-search-experiment/attention-magic-values.log
--- reset.sh
+++ reset.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp bin:lib/RXTXcomm.jar:lib/log4j-1.2.jar:. -Dgnu.io.rxtx.SerialPorts=/dev/ttyUSB0:/dev/ttyUSB1 uk.ac.lancs.relate.SerialConnector /dev/ttyUSB0
+java -cp bin:/usr/share/java/RXTXcomm.jar:lib/log4j-1.2.jar:. -Dgnu.io.rxtx.SerialPorts=/dev/ttyUSB0:/dev/ttyUSB1 -Djava.library.path=/usr/lib uk.ac.lancs.relate.SerialConnector /dev/ttyUSB1
