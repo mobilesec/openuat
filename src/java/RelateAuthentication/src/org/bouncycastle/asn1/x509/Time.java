a62e0f9cbfd70d7c83bf6038de4a1a73918f8560
Rene Mayrhofer <rene@mayrhofer.eu.org>
2006-03-17 20:58:34 +0000

    Wow. This creates a certificate in PKCS12 format that can actually be read by openssl:
    ant jar && java -cp dist/spatial-authentication.jar:lib/log4j-1.2.jar:. org.eu.mayrhofer.channel.X509CertificateGenerator && openssl pkcs12 -nodes -info -in test.p12 > /tmp/test.cert && openssl x509 -noout -text -in /tmp/test.cert
    
    Works!
    
    Now it needs improvement to read a CA private key and certificate and use that
    one instead of creating a self-signed certificate....
    
    
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@227 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/java/RelateAuthentication/src/org/bouncycastle/asn1/x509/Time.java
+++ src/java/RelateAuthentication/src/org/bouncycastle/asn1/x509/Time.java
@@ -1,11 +1,20 @@
 package org.bouncycastle.asn1.x509;
 
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.SimpleTimeZone;
 
-import org.bouncycastle.asn1.*;
+import org.bouncycastle.asn1.ASN1Choice;
+import org.bouncycastle.asn1.ASN1Encodable;
+import org.bouncycastle.asn1.ASN1TaggedObject;
+import org.bouncycastle.asn1.DERGeneralizedTime;
+import org.bouncycastle.asn1.DERObject;
+import org.bouncycastle.asn1.DERUTCTime;
 
 public class Time
-    implements DEREncodable, ASN1Choice
+    extends ASN1Encodable
+    implements ASN1Choice
 {
     DERObject   time;
 
@@ -13,7 +22,7 @@ public class Time
         ASN1TaggedObject obj,
         boolean          explicit)
     {
-        return getInstance(obj.getObject());
+        return getInstance(obj.getObject()); // must be explicitly tagged
     }
 
     public Time(
@@ -26,6 +35,32 @@ public class Time
         }
 
         this.time = time; 
+    }
+
+    /**
+     * creates a time object from a given date - if the date is between 1950
+     * and 2049 a UTCTime object is generated, otherwise a GeneralizedTime
+     * is used.
+     */
+    public Time(
+        Date    date)
+    {
+        SimpleTimeZone      tz = new SimpleTimeZone(0, "Z");
+        SimpleDateFormat    dateF = new SimpleDateFormat("yyyyMMddHHmmss");
+
+        dateF.setTimeZone(tz);
+
+        String  d = dateF.format(date) + "Z";
+        int     year = Integer.parseInt(d.substring(0, 4));
+
+        if (year < 1950 || year > 2049)
+        {
+            time = new DERGeneralizedTime(d);
+        }
+        else
+        {
+            time = new DERUTCTime(d.substring(2));
+        }
     }
 
     public static Time getInstance(
@@ -59,14 +94,22 @@ public class Time
         }
     }
 
+    public Date getDate()
+    {
+        SimpleDateFormat dateF = new SimpleDateFormat("yyyyMMddHHmmssz");
+
+        return dateF.parse(this.getTime(), new ParsePosition(0));
+    }
+
     /**
+     * Produce an object suitable for an ASN1OutputStream.
      * <pre>
      * Time ::= CHOICE {
      *             utcTime        UTCTime,
      *             generalTime    GeneralizedTime }
      * </pre>
      */
-    public DERObject getDERObject()
+    public DERObject toASN1Object()
     {
         return time;
     }
