72b644022fd76b9ecf46bfc0da85c110ef3c23fa
Rene Mayrhofer <rene@mayrhofer.eu.org>
2006-03-22 23:02:54 +0000

    Added a new convenience class for streaming a binary block over a connection that's already open. TODO: - make the unit test work (it's too late right now...) - change the Reader/Writer combination to the more general   InputStream/OutputStream.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@245 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ src/java/RelateAuthentication/src/org/eu/mayrhofer/apps/BinaryBlockStreamer.java
@@ -0,0 +1,133 @@
+package org.eu.mayrhofer.apps;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+import org.apache.log4j.Logger;
+
+/** This is a helper class for streaming binary blocks over some (byte-safe) connection.
+ * It can e.g. be used to stream a file over a TCP connection that has been opened previsouly.
+ * The sender side can prefix the binary block with a name so that the receiver side can
+ * distinguish different types of binary blocks in a protocol. This name prefix as well as
+ * the length of the following binary block in bytes is prefixing the block itself as a
+ * complete line terminated with "\n".
+ * 
+ * @author Rene Mayrhofer
+ */
+public class BinaryBlockStreamer {
+	/** Our log4j logger. */
+	private static Logger logger = Logger.getLogger(BinaryBlockStreamer.class);
+	
+	private final static String BinaryStreamCommand = "PUSH";
+
+	/** The input from which to read binary blocks. It may be null if this object is 
+	 * only used for writing.
+	 */
+	private Reader input;
+	/** The output to which binary blocks should be written. It may be null if this object is 
+	 * only used for reading.
+	 */
+	private Writer output;
+	
+	/** Initializes the object with input and/or output. At least one of them must be
+	 * set.
+	 * 
+	 * @param input The input from which to read, if this object is to be used for 
+	 *              receiving binary blocks from a channel. 
+	 *              It can be null, but then output must be set. 
+	 * @param output The output to write to, if this object is to be used for 
+	 *               sending binary blocks to a channel. 
+	 *               It can be null, but then input must be set. 
+	 */
+	public BinaryBlockStreamer(Reader input, Writer output) {
+		this.input = input;
+		this.output = output;
+		if (input == null && output == null) {
+			logger.error("Need at least either input or output to be set");
+			throw new IllegalArgumentException("Need either input or output (or both) to be set");
+		}
+	}
+	
+	/** Send a binary block over the output that has been passed to the constructor.
+	 * 
+	 * @param blockName The name that should be prefixed before sending the block.
+	 * @param block The block itself, represented as InputStream. This stream is read from the beginning.
+	 * @param size The size of the block to send, i.e. how many bytes of block should be read and sent to output.
+	 * @throws IOException When something does not work as expected, i.e. when the bytes can not be sent,
+	 *                     an IOException is thrown (or passed through from the underlying streams).
+	 */
+	public void sendBinaryBlock(String blockName, InputStream block, int size) throws IOException {
+		if (output == null) {
+			logger.error("Can't send binary block as no output has been defined");
+			throw new IOException("Output has not been defined, can not send");
+		}
+		if (blockName == null || block == null || size <= 0) {
+			throw new IllegalArgumentException("Got illegal argument");
+		}
+		
+		logger.info("Sending binary block with " + size + "B named '" + blockName + "'");
+		output.write(BinaryStreamCommand + " " + size + " " + blockName + "\n");
+		for (int i=0; i<size; i++)
+			output.write(block.read());
+		logger.info("Successfully finished sending binary block");
+	}
+	
+	/** Receive a binary block from the input that has been passed to the constructor.
+	 * 
+	 * @param blockName The name of the block that has been received in the prefix. This
+	 *                  parameter should point to an empty StringBuffer object, to which the
+	 *                  name will be appended (also known as poor/Java man's output parameter).
+	 * @param block The block itself, represented as OutputStream. This stream is written to.
+	 * @return size The number of bytes that have actually been received, if it was possible to
+	 *              receive the number of bytes that the sender of the block specified in its
+	 *              prefix line, If less then this intended number was received, the number of bytes 
+	 *              that have been received is returned as a negative number. E.g. when the sender
+	 *              stated that 20 bytes would be sent, but only 15 could be received before an end
+	 *              of file or other read error occured, this method will return -15. In short, any
+	 *              return value >0 indicates an successful receive of the whole block and specifies
+	 *              the number of bytes that have been written to block. Any number <=0 indicates an
+	 *              error.
+	 * @throws IOException When something does not work as expected, i.e. when the bytes can not be received,
+	 *                     an IOException is thrown (or passed through from the underlying streams).
+	 */
+	public int receiveBinaryBlock(StringBuffer blockName, OutputStream block) throws IOException {
+		if (input == null) {
+			logger.error("Can't receive binary block as no input has been defined");
+			throw new IOException("Input has not been defined, can not receive");
+		}
+		if (blockName == null || block == null) {
+			throw new IllegalArgumentException("Got illegal argument");
+		}
+		
+		// we can only go on if we actually get a proper start line
+		logger.debug("Trying to get prefix line");
+		String prefixLine = new BufferedReader(input).readLine();
+		if (prefixLine == null || ! prefixLine.startsWith(BinaryStreamCommand)) {
+			logger.error("Did not receive properly formatted streaming command line while trying to receive binary block. Received '" + prefixLine + "'");
+			return -1;
+		}
+		logger.debug("Received prefix line '" + prefixLine + "'");
+		// try to decode the two parameters: first the size, then the name
+		int offset = prefixLine.indexOf(' ', BinaryStreamCommand.length()+1);
+		int intendedSize = Integer.parseInt(prefixLine.substring(BinaryStreamCommand.length()+1, offset));
+		blockName.append(prefixLine.substring(offset+1, prefixLine.length()));
+		logger.info("Receiving binary block with " + intendedSize + "B named '" + blockName + "'");
+		int i=0, buf;
+		do {
+			buf = input.read();
+			logger.debug("Read " + buf);
+			if (buf != -1)
+				block.write(buf);
+			i++;
+		} while (i<intendedSize && buf != -1);
+		if (i == intendedSize)
+			logger.info("Successfully finished receiving binary block");
+		else
+			logger.error("Could not receive all requested " + intendedSize + "B, only got " + i + "B before end of file or read error");
+		return i;
+	}
+}
---
+++ src/java/RelateAuthentication/src/org/eu/mayrhofer/apps/test/BinaryBlockStreamerTest.java
@@ -0,0 +1,90 @@
+package org.eu.mayrhofer.apps.test;
+
+import java.io.*;
+
+import org.eu.mayrhofer.apps.BinaryBlockStreamer;
+
+import junit.framework.*;
+
+public class BinaryBlockStreamerTest extends TestCase {
+	public BinaryBlockStreamerTest(String s) {
+		super(s);
+	}
+
+	public void setUp() {
+		// nothing to do here, no sensible defaults
+	}
+
+	public void tearDown() {
+		// nothing to do here
+	}
+	
+	public void testEitherInputOrOutputCheck() {
+		try {
+			BinaryBlockStreamer streamer = new BinaryBlockStreamer(null, null);
+		} catch (IllegalArgumentException e) {
+			// we expect this
+			return;
+		}
+		Assert.fail("Constructor accepted null input and output");
+	}
+	
+	public void testInputNotNullForReceiveCheck() {
+		try {
+			BinaryBlockStreamer streamer = new BinaryBlockStreamer(null, new StringWriter());
+			streamer.receiveBinaryBlock(new StringBuffer(), new ByteArrayOutputStream());
+		} catch (IOException e) {
+			// we expect this
+			return;
+		}
+		Assert.fail("Constructor accepted null input and output");
+	}
+
+	public void testOutputNotNullForSendCheck() {
+		try {
+			BinaryBlockStreamer streamer = new BinaryBlockStreamer(new StringReader("test"), null);
+			streamer.sendBinaryBlock("test", new ByteArrayInputStream(new byte[] {0}), 1);
+		} catch (IOException e) {
+			// we expect this
+			return;
+		}
+		Assert.fail("Constructor accepted null input and output");
+	}
+	
+	private boolean compareByteArrays(byte[] b1, byte[] b2) {
+		if (b1.length != b2.length)
+			return false;
+		for (int i=0; i<b1.length; i++)
+			if (b1[i] != b2[i])
+				return false;
+		return true;
+	}
+	
+	public void testSendAndReceive() throws IOException {
+		// need to BinaryBlockStreamers, coupled via a pipe (it's the easiest)
+		PipedWriter writePipe = new PipedWriter();
+		PipedReader readPipe = new PipedReader(writePipe);
+		BinaryBlockStreamer src = new BinaryBlockStreamer(null, writePipe);
+		BinaryBlockStreamer dst = new BinaryBlockStreamer(readPipe, null);
+		
+		// two test cases: send a binary array and a string
+		// this tests that the streams are left open and that they are indexed properly
+		// this includes CR and LF characters and a null character - so it should test for byte safety
+		byte[] test1 = new byte[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+		String test1name = "bytetest";
+		String test2 = "my string test block with\nsome\nnewlines\nand\n\t\ttabs\nmixed in.";
+		String test2name = "string test name with blanks";
+		src.sendBinaryBlock(test1name, new ByteArrayInputStream(test1), test1.length);
+		StringBuffer recvName = new StringBuffer();
+		ByteArrayOutputStream test1rcv = new ByteArrayOutputStream();
+		Assert.assertEquals("Did not receive the same amount of bytes as have been sent", test1.length, dst.receiveBinaryBlock(recvName, test1rcv));
+		Assert.assertEquals("THe received block name does not match the sent one", test1name, recvName.toString());
+		Assert.assertTrue("Received binary array does not match the sent one", compareByteArrays(test1, test1rcv.toByteArray()));
+		src.sendBinaryBlock(test1name, new ByteArrayInputStream(test2.getBytes()), test2.length());
+		recvName = new StringBuffer();
+		ByteArrayOutputStream test2rcv = new ByteArrayOutputStream();
+		Assert.assertEquals("Did not receive the same amount of bytes as have been sent", test2.length(), dst.receiveBinaryBlock(recvName, test2rcv));
+		Assert.assertEquals("THe received block name does not match the sent one", test2name, recvName.toString());
+		Assert.assertEquals("Received string does not match the sent one", test2, test2rcv.toString());
+	}
+}
