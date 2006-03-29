e8011a054ece650e86f193bbe010c369902acf4f
Rene Mayrhofer <rene@mayrhofer.eu.org>
2006-03-29 13:09:23 +0000

    This is the first commit of a restructuring of the source tree: - Move all shell scripts to the new directory scripts/ and change the paths accordingly. - Split the JAR file generation in build.xml to distinguish into:   - core   - secure channels   - applications   and also create a bundle of all of them including their dependencies. - Start moving the thirdparty-but-maybe-modified Java source classes from src/ to   thirdparty/bouncycastle-*-deps, depending on which of the JAR files needs them. This will allow the core JAR to stay very small when secure channels are not needed (and the secure channels are quite independent of the spatial authentication anyway).
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@268 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/java/RelateAuthentication/runme.sh
+++
@@ -1,2 +0,0 @@
-# classpath . is just so that log4j.properties is found
-java -cp .:bin:lib/relate-dist.jar:lib/swt-3.1.jar -Djava.library.path=../relate/thirdparty/nativelib/linux org.eu.mayrhofer.authentication.RelateAuthenticationProtocol $@
