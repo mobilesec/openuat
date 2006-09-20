ec5baf9aaeaf957b3cbae241d3686bf296ec64c4
Rene Mayrhofer <rene@mayrhofer.eu.org>
2006-09-20 16:55:44 +0000

    - The multiplicator is dependent on the sensor, in this case 1/512 for WiTilt. - Add avetana to the Java classpath.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk/src/java/openuat@537 01b9256e-9321-419f-88ee-03bd83f56c6c

--- scripts/run-shaking-demo.sh
+++ scripts/run-shaking-demo.sh
@@ -1,1 +1,1 @@
-java -cp dist/openuat-distbundle.jar:lib/linux/swt-3.1.1_gtk.linux.x86.jar -Djava.library.path=nativelib/linux -Dos.name=Linux-all-ports org.eu.mayrhofer.apps.ShakingSinglePCDemonstrator $@
+java -cp src/:dist/openuat-distbundle.jar:lib/linux/swt-3.1.1_gtk.linux.x86.jar:lib/avetanaBT.jar -Djava.library.path=nativelib/linux -Dos.name=Linux-all-ports org.eu.mayrhofer.apps.ShakingSinglePCDemonstrator $@
