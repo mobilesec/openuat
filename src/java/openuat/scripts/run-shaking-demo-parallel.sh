a7ecfcbe9a9b094151436d5e4a1166f976cb4c29
Rene Mayrhofer <rene@mayrhofer.eu.org>
2006-10-06 15:28:06 +0000

    - Add a script (and the parport-pulsewidth binary) for quickly staring the single PC shaking demo.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@591 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ src/java/openuat/scripts/run-shaking-demo-parallel.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# where are we called from?
+mydir=`dirname $0`
+
+JAR_PATH=$mydir/../dist/openuat-distbundle.jar:$mydir/../lib/jfreechart-1.0.1.jar:$mydir/../lib/jcommon-1.0.0.jar:$mydir/../lib/linux/swt-3.1.1_gtk.linux.x86.jar
+
+java -cp ${JAR_PATH} -Djava.library.path=$mydir/../nativelib/linux org.eu.mayrhofer.apps.ShakingSinglePCDemonstrator listentcp 56677 &
+
+# wait for the GUI to start and the TCP port to be opened for listening
+sleep 3s
+
+# and start the sampling
+sudo $mydir/parport-pulsewidth - | nc -q0 localhost 56677
