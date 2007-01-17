5dfc62c2300a250a014e4b4c7e8493ee39bbfd97
Rene Mayrhofer <rene@mayrhofer.eu.org>
2007-01-17 23:31:54 +0000

    - Added a small demo class on how to use animated tray icons with JRE   6.0.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@612 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ src/org/openuat/apps/util/TrayIconHelper.java
@@ -0,0 +1,60 @@
+package org.openuat.apps.util;
+
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class TrayIconHelper extends TimerTask implements ActionListener {
+	
+	private static final int WIDTH = 16;
+	private static final int HEIGHT = 16;
+	
+	private boolean isBlue;
+	private TrayIcon trayIcon;
+	private Dimension iconSize;
+	private BufferedImage image;
+	
+	public TrayIconHelper() {
+		isBlue = true;
+		SystemTray systemTray = SystemTray.getSystemTray();
+		iconSize = systemTray.getTrayIconSize();
+		image = new BufferedImage(iconSize.width, iconSize.height, BufferedImage.TYPE_INT_RGB);
+		trayIcon = new TrayIcon(image, "double-click to exit");
+		trayIcon.addActionListener(this);
+		run();
+		try {
+			systemTray.add(trayIcon);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+		Timer timer = new Timer();
+		timer.schedule(this, 1000, 1000);
+	}
+	
+	public void run() {
+		Graphics g = image.getGraphics();
+		g.setColor(Color.white);
+		g.fillRect(0, 0, iconSize.width, iconSize.height);
+		g.setColor(isBlue ? Color.blue : Color.black);
+		g.fillArc(0, 0, iconSize.width - 1, iconSize.height - 1, 0, 360);
+		isBlue = ! isBlue;
+		trayIcon.setImage(image);
+	}
+
+	public static void main(String[] args) {
+		if (SystemTray.isSupported())
+			new TrayIconHelper();
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		System.exit(0);
+	}
+}
