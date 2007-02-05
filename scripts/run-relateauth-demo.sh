16471e42b90258078fe5201eb125dc51f4741249
Rene Mayrhofer <rene@mayrhofer.eu.org>
2007-02-05 11:44:50 +0000

    - The script now also tries to auto-locate dongles in addition to bricks.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@631 01b9256e-9321-419f-88ee-03bd83f56c6c

--- scripts/run-relateauth-demo.sh
+++ scripts/run-relateauth-demo.sh
@@ -3,18 +3,37 @@ if [ x"$1" = x"autorun-once" ]; then
   # detect relate devices and take the first two
   dev1=""
   dev2=""
+  # first look for bricks
   for id in `grep "FT232R USB UART" /sys/bus/usb/drivers/usb/*/product | awk -F'/' '{print $7}'`; do
     dev=`basename /sys/bus/usb/drivers/ftdi_sio/$id:1.0/ttyUSB*`
-    if [ -z "$dev1" ]; then dev1=$dev
-    elif [ -z "$dev2" ]; then dev2=$dev
+    if [ -z "$dev1" ]; then dev1=$dev; dev1type=2; echo "/dev/$dev1 seems to be a Relate brick"
+    elif [ -z "$dev2" ]; then dev2=$dev; dev2type=2; echo "/dev/$dev2 seems to be a Relate brick"
     # ignore others
     else echo "Ignoring device $dev"
     fi
   done
+  if [ -n "$dev1" -a -n "$dev2" ]; then
+    echo "Found two Relate bricks on /dev/$dev1 and /dev/$dev2, using them"
+  else
+    # didn't find two bricks, looking for dongles now
+    for id in `grep "USB <-> Serial" /sys/bus/usb/drivers/usb/*/product | awk -F'/' '{print $7}'`; do
+      dev=`basename /sys/bus/usb/drivers/ftdi_sio/$id:1.0/ttyUSB*`
+      if [ -z "$dev1" ]; then dev1=$dev; dev1type=1; echo "/dev/$dev1 seems to be a Relate dongle"
+      elif [ -z "$dev2" ]; then dev2=$dev; dev2type=1; echo "/dev/$dev2 seems to be a Relate dongle"
+      # ignore others
+      else echo "Ignoring device $dev"
+      fi
+    done
+  fi
 
-  echo "Using devices $dev1 and $dev2"
-  echo
-  params="both /dev/$dev1 2 /dev/$dev2 2 5"
+  if [ -n "$dev1" -a -n "$dev2" ]; then
+    echo "Found two Relate devices on /dev/$dev1 (type $dev1type) and /dev/$dev2 (type $dev2type), using them"
+    echo
+    params="both /dev/$dev1 $dev1type /dev/$dev2 $dev2type 5"
+  else
+    echo "Could not find two Relate devices, aborting"
+    exit 1
+  fi
 else
   params=$@
 fi
