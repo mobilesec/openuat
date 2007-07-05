cf095952398bab6e7863d4484fd491b0ec5fe6b2
Rene Mayrhofer <rene@mayrhofer.eu.org>
2007-07-05 21:44:24 +0000

    - Some code for using Thinkpad accelerometers under Windows. - Dummy Correlation and integer coherence classes.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@770 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ src/org/openuat/features/Correlation.java
@@ -0,0 +1,31 @@
+/* Copyright Rene Mayrhofer
+ * File created 2007-06-06
+ * 
+ * This implementation is based on the "cohere", "pwelch", "hanning", 
+ * and "conj" functions in Octave and Octave Forge.
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+package org.openuat.features;
+
+public class Correlation {
+	// http://en.wikipedia.org/wiki/Correlation
+	public int correlationCoefficient(int[] a, int[] b, int offset) {
+		return 0;
+	}
+	
+	public double correlationCoefficient(double[] a, double[] b, int offset) {
+		return 0;
+	}
+
+	public int maxCorrelationCoefficient(int[] a, int[] b, int maxOffsetSearch) {
+		return 0;
+	}
+
+	public double maxCorrelationCoefficient(double[] a, double[] b, int maxOffsetSearch) {
+		return 0;
+	}
+}
