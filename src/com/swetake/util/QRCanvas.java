be696441842c041949626f98c97c7bf6370802a4
Iulia Ion <iulia.ion@gmail.com>
2008-07-14 16:16:29 +0000

    QRCode generator for j2se
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@966 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ src/com/swetake/util/QRCanvas.java
@@ -0,0 +1,42 @@
+package com.swetake.util;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ * Simple canvas to quickly display the QR Code result on J2SE
+ * @author Iulia Ion
+ *
+ */
+class QRCanvas extends Canvas{
+	boolean [][] s;
+	public QRCanvas(boolean [][] qrcode){
+		this.s = qrcode;
+	}
+    public QRCanvas() {
+		// TODO Auto-generated constructor stub
+	}
+	public void paint(Graphics g) {
+		g.setColor(Color.black);
+		
+
+	    for (int i=0;i<s.length;i++){
+			for (int j=0;j<s.length;j++){
+			    if (s[j][i]) {
+			    	g.fillRect(j*10,i*10,10,10);
+			    	//System.out.print("X");
+			    }
+			    //else System.out.print("O");
+			}
+			//System.out.println();
+		    }
+    	
+    }
+	public boolean[][] getS() {
+		return s;
+	}
+	public void setS(boolean[][] s) {
+		this.s = s;
+	}
+
+}
