884399837f032818b980e072e88abed3fab1cde4
Rene Mayrhofer <rene@mayrhofer.eu.org>
2008-10-09 12:10:16 +0000

    - Add Iulia's poster. - A new script for recording shaking data sets.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@1070 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ scripts/experiment-5500.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ADDR1=00180FA4C997
+ADDR2=00180FA3A1D4
+
+if [ $# -ne 1 ]; then
+  echo "Error: need log prefix"
+  exit 1
+fi
+
+if [ -e /tmp/$1 ]; then
+  echo "Error: /tmp/$1 already exists"
+  exit 2
+fi
+
+mkdir /tmp/$1
+
+java -cp `dirname $0`/../dist/openuat-distbundle.jar org.openuat.util.BluetoothRFCOMMChannel $ADDR1 2 stream | /srv/debian-unstable-i386/usr/bin/tai64n | /srv/debian-unstable-i386/usr/bin/tai64nlocal > /tmp/$1/1 &
+
+java -cp `dirname $0`/../dist/openuat-distbundle.jar org.openuat.util.BluetoothRFCOMMChannel $ADDR2 2 stream | /srv/debian-unstable-i386/usr/bin/tai64n | /srv/debian-unstable-i386/usr/bin/tai64nlocal > /tmp/$1/2 &
+
+echo "Press enter to terminate"
+read
+
+killall java
