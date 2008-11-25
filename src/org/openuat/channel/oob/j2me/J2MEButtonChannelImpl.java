fe158c69f640d50fe69854008c106ff1da00fdc8
Lukas Huser <lhuser@student.ethz.ch>
2008-11-25 14:45:32 +0000

    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@1093 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/org/openuat/channel/oob/j2me/J2MEButtonChannelImpl.java
+++ src/org/openuat/channel/oob/j2me/J2MEButtonChannelImpl.java
@@ -8,7 +8,12 @@
  */
 package org.openuat.channel.oob.j2me;
 
+import javax.microedition.lcdui.Canvas;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Graphics;
 
 import org.openuat.channel.oob.ButtonChannelImpl;
 import org.openuat.channel.oob.ButtonInputHandler;
@@ -20,7 +25,7 @@ import org.openuat.channel.oob.ButtonInputHandler;
  * @author Lukas Huser
  * @version 1.0
  */
-public class J2MEButtonChannelImpl extends ButtonChannelImpl {
+public class J2MEButtonChannelImpl extends ButtonChannelImpl implements CommandListener {
 
 	/**
 	 * Creates a new instance.
@@ -32,45 +37,175 @@ public class J2MEButtonChannelImpl extends ButtonChannelImpl {
 	}
 	
 	/**
-	 * main applications <code>Display</code>
+	 * Main applications <code>Display</code>.
 	 */
 	protected Display display;
+	
+	/**
+	 * Current gui element displayed on screen.
+	 */
+	protected Canvas currentScreen;
+	
+	/**
+	 * Allows the user to cancel the currently displayed screen.
+	 */
+	protected Command abortCommand;
 
 	/* (non-Javadoc)
 	 * @see org.openuat.channel.oob.ButtonChannelImpl#repaint()
 	 */
 	@Override
 	public void repaint() {
-		// TODO Auto-generated method stub
-		
+		if (currentScreen != null) {
+			currentScreen.repaint();
+		}
+		else {
+			// TODO: log warning
+			// logger.warn("Call to repaint(), but currentScreen is null.");
+		}
 	}
 
 	/* (non-Javadoc)
-	 * @see org.openuat.channel.oob.ButtonChannelImpl#showCaptureGui(java.lang.String, org.openuat.channel.oob.ButtonInputHandler)
+	 * @see org.openuat.channel.oob.ButtonChannelImpl#showCaptureGui
 	 */
 	@Override
 	public void showCaptureGui(String text, ButtonInputHandler inputHandler) {
-		// TODO Auto-generated method stub
+		abortCommand = new Command("Abort", Command.STOP, 1);
+		currentScreen = new CaptureGui(text, inputHandler);
+		currentScreen.addCommand(abortCommand);
+		currentScreen.setCommandListener(this);
 		
+		// make currentScreen the active Displayable
+		display.setCurrent(currentScreen);
 	}
 
 	/* (non-Javadoc)
-	 * @see org.openuat.channel.oob.ButtonChannelImpl#showTransmitGui(java.lang.String, int)
+	 * @see org.openuat.channel.oob.ButtonChannelImpl#showTransmitGui
 	 */
 	@Override
 	public void showTransmitGui(String text, int type) {
-		// TODO Auto-generated method stub
-		
+		transmissionMode = type;
+		// TODO: show gui
 	}
 
 	/* (non-Javadoc)
-	 * @see org.openuat.channel.oob.ButtonChannelImpl#vibrate(int)
+	 * @see org.openuat.channel.oob.ButtonChannelImpl#vibrate
 	 */
 	@Override
 	public void vibrate(int milliseconds) {
 		display.vibrate(milliseconds);
 	}
-
 	
+	/* (non-Javadoc)
+	 * @see javax.microedition.lcdui.CommandListener#commandAction(javax.microedition.lcdui.Command, javax.microedition.lcdui.Displayable)
+	 */
+	@Override
+	public void commandAction(Command command, Displayable displayable) {
+		if (command.getCommandType() == Command.STOP) {
+			// TODO: abort current processing...
+		}
+		else {
+			// TODO: log warning
+			// logger.warn("Command not handled: "+command.getLabel());
+		}
+	}
 
+
+
+	/*
+	 * Private helper/wrapper class to launch the capture gui.
+	 */
+	private class CaptureGui extends Canvas {
+		
+		/*
+		 * Constructor for this class.
+		 */
+		public CaptureGui(String displayText, ButtonInputHandler handler) {
+			this.displayText = displayText;
+			this.inputHandler = handler;
+			marginLeft	= 10;
+			marginTop	= 10;
+		}
+		
+		/* Text to display on gui */
+		private String displayText;
+		
+		/* Delegate key events to the input handler */
+		private ButtonInputHandler inputHandler;
+		
+		/* Margin values when drawing on the screen */
+		private int marginLeft;
+		private int marginTop;
+		
+		/* (non-Javadoc)
+		 * @see javax.microedition.lcdui.Canvas#paint(Graphics)
+		 */
+		@Override
+		protected void paint(Graphics g) {
+			g.drawString(displayText, marginLeft, marginTop, Graphics.TOP|Graphics.LEFT);
+		}
+
+		/* (non-Javadoc)
+		 * @see javax.microedition.lcdui.Canvas#keyPressed(int)
+		 */
+		@Override
+		protected void keyPressed(int keyCode) {
+			if (getGameAction(keyCode) == Canvas.FIRE) {
+				inputHandler.buttonPressed();
+			}
+		}
+
+		/* (non-Javadoc)
+		 * @see javax.microedition.lcdui.Canvas#keyReleased(int)
+		 */
+		@Override
+		protected void keyReleased(int keyCode) {
+			if (getGameAction(keyCode) == Canvas.FIRE) {
+				inputHandler.buttonReleased();
+			}
+		}
+	}
+	
+	
+	private class TransmitGui extends Canvas {
+		
+		/*
+		 * Constructor for this class.
+		 */
+		public TransmitGui(String displayText, int transmissionMode) {
+			this.displayText = displayText;
+			this.transmissionMode = transmissionMode;
+			marginLeft	= 10;
+			marginTop	= 10;
+		}
+		
+		/* Text to display before transmission starts */
+		private String displayText;
+		
+		/* current transmission mode
+		 * @see ButtonChannelImpl#transmissionMode
+		 */
+		private int transmissionMode;
+		
+		/* Margin values when drawing on the screen */
+		private int marginLeft;
+		private int marginTop;
+
+		/* (non-Javadoc)
+		 * @see javax.microedition.lcdui.Canvas#paint(javax.microedition.lcdui.Graphics)
+		 */
+		@Override
+		protected void paint(Graphics g) {
+			if (transmissionMode == ButtonChannelImpl.TRANSMIT_PLAIN) {
+				g.drawString(displayText, marginLeft, marginTop, Graphics.LEFT|Graphics.TOP);
+			}
+			else if (transmissionMode == ButtonChannelImpl.TRANSMIT_SIGNAL) {
+				// TODO: something...
+			}
+			
+		}
+		
+		
+	}
+	
 }
