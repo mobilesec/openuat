c8a2d44400944ab943f0ccd0f8be558f7622005e
Lukas Huser <lhuser@student.ethz.ch>
2008-11-26 17:43:52 +0000

    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@1096 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/org/openuat/channel/oob/AWTButtonChannelImpl.java
+++ src/org/openuat/channel/oob/AWTButtonChannelImpl.java
@@ -8,8 +8,12 @@
  */
 package org.openuat.channel.oob;
 
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-
+import javax.swing.JComponent;
+import javax.swing.JTextPane;
 
 
 /**
@@ -24,17 +28,41 @@ public class AWTButtonChannelImpl extends ButtonChannelImpl {
 	/**
 	 * Creates a new instance.
 	 */
-	public AWTButtonChannelImpl() {
-		// TODO: initialize
+	public AWTButtonChannelImpl(JComponent parentComponent) {
+		transmissionMode	= 0;
+		progress			= 0;
+		showSignal			= false;
+		buttonInputHandler	= null;
+		intervalList		= null;
+		currentComponent	= null;
+		parent				= parentComponent;
 	}
+	
+	/**
+	 * The parent gui component.
+	 */
+	protected JComponent parent;
+	
+	/**
+	 * The current gui element displayed on screen.
+	 */
+	protected JComponent currentComponent;
+	
+	/* Keep the inputHandler as a private member, so an anonymous class can access it */
+	private ButtonInputHandler buttonInputHandler;
 
 	/* (non-Javadoc)
 	 * @see org.openuat.channel.oob.ButtonChannelImpl#repaint()
 	 */
 	@Override
 	public void repaint() {
-		// TODO Auto-generated method stub
-		
+		if (currentComponent != null) {
+			currentComponent.repaint();
+		}
+		else {
+			// TODO: log warning
+			// logger.warn("Method repaint(): currentComponent is null");
+		}
 	}
 
 	/* (non-Javadoc)
@@ -42,8 +70,38 @@ public class AWTButtonChannelImpl extends ButtonChannelImpl {
 	 */
 	@Override
 	public void showCaptureGui(String text, ButtonInputHandler inputHandler) {
-		// TODO Auto-generated method stub
+		buttonInputHandler = inputHandler;
+		JTextPane captureGui = new JTextPane();
+		KeyListener keyListener = new KeyAdapter() {
+
+			/* (non-Javadoc)
+			 * @see java.awt.event.KeyAdapter#keyPressed(java.awt.event.KeyEvent)
+			 */
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					buttonInputHandler.buttonPressed();
+				}
+			}
+
+			/* (non-Javadoc)
+			 * @see java.awt.event.KeyAdapter#keyReleased(java.awt.event.KeyEvent)
+			 */
+			@Override
+			public void keyReleased(KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+					buttonInputHandler.buttonReleased();
+				}
+			}
+		};
 		
+		captureGui.addKeyListener(keyListener);
+		captureGui.setText(text);
+		
+		// display currentComponent
+		currentComponent = captureGui;
+		parent.add(currentComponent);
+		parent.repaint();
 	}
 
 	/* (non-Javadoc)
@@ -64,6 +122,7 @@ public class AWTButtonChannelImpl extends ButtonChannelImpl {
 		// TODO Logger.warn("method vibrate is not implemented on AWT");
 		
 	}
+	
 	
 
 
