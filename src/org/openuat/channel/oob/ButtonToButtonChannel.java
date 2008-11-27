c8e27278fe758d714990145f1b702ddbed4dddff
Lukas Huser <lhuser@student.ethz.ch>
2008-11-27 15:46:28 +0000

    initial version
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@1098 01b9256e-9321-419f-88ee-03bd83f56c6c

---
+++ src/org/openuat/channel/oob/ButtonToButtonChannel.java
@@ -0,0 +1,49 @@
+/* Copyright Lukas Huser
+ * File created 2008-11-27
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+package org.openuat.channel.oob;
+
+/**
+ * This channel is an <b>input channel</b>, it only implements the
+ * <code>capture</code> method and doesn't support the <code>transmit</code>
+ * method (it just does nothing).<br/>
+ * This channel only records button presses and ignores button releases.<br/>
+ * The smallest considered time unit is set to 300 milliseconds
+ * (See C. Soriente, G. Tsudik: 'BEDA: Button-Enabled Device Association' for more
+ * details).
+ * 
+ * @author Lukas Huser
+ * @version 1.0
+ */
+public class ButtonToButtonChannel extends ButtonChannel {
+	
+	public ButtonToButtonChannel(ButtonChannelImpl impl) {
+		this.impl		= impl;
+		minTimeUnit		= 300;
+		inputMode		= MODE_PRESS;
+		doRoundDown		= false;
+		useCarry		= true;
+		messageHandler	= null;
+		
+		int eventCount = (MESSAGE_LENGTH / BITS_PER_INTERVAL) + 1;
+		captureDisplayText	= "Please press the button simultanously on both devices "
+							+ "for a total of " + Integer.toString(eventCount) + " times.\n"
+							+ "This device is ready.";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.openuat.authentication.OOBChannel#transmit(byte[])
+	 */
+	@Override
+	public void transmit(byte[] message) {
+		// This method is not implemented!
+		// TODO: log warning
+		// logger.warn("Method transmit(byte[]): Not implemented for the ButtonToButtonChannel.");
+	}
+
+}
