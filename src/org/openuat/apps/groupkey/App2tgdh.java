73c0469a3e92b6f42773971d9c256369943ffef8
Rene Mayrhofer <rene@mayrhofer.eu.org>
2009-01-19 11:58:50 +0000

    - Add copyright header.
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@1202 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/org/openuat/apps/groupkey/App2tgdh.java
+++ src/org/openuat/apps/groupkey/App2tgdh.java
@@ -1,3 +1,12 @@
+/* Copyright Martijn Sack
+ * File created 2009-01-18
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 package org.openuat.apps.groupkey;
 
 import java.io.NotSerializableException;
