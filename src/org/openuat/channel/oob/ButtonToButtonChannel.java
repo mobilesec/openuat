175bdd02e67e5f6cf2b4b6ba8de2fd35615a663d
Lukas Huser <lhuser@student.ethz.ch>
2009-02-13 09:42:25 +0000

    reaction delay set to 400 ms
    git-svn-id: file:///srv/svn/openuat-filtered-without-thirdparty/trunk@1255 01b9256e-9321-419f-88ee-03bd83f56c6c

--- src/org/openuat/channel/oob/ButtonToButtonChannel.java
+++ src/org/openuat/channel/oob/ButtonToButtonChannel.java
@@ -15,7 +15,7 @@ import org.openuat.log.LogFactory;
  * <code>capture</code> method and doesn't support the <code>transmit</code>
  * method (it just does nothing).<br/>
  * This channel only records button presses and ignores button releases.<br/>
- * The smallest considered time unit is set to 350 milliseconds
+ * The smallest considered time unit is set to 400 milliseconds
  * (See C. Soriente, G. Tsudik: 'BEDA: Button-Enabled Device Association' for more
  * details).
  * 
@@ -32,7 +32,7 @@ public class ButtonToButtonChannel extends ButtonChannel {
 	 */
 	public ButtonToButtonChannel(ButtonChannelImpl impl) {
 		this.impl		= impl;
-		minTimeUnit		= 350;
+		minTimeUnit		= 400;
 		inputMode		= MODE_PRESS;
 		doRoundDown		= false;
 		useCarry		= false;
